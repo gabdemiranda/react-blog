da30127ee13c2a92128590c957d13e7e03c47752
gabdemiranda <gabriel.demcastro@gmail.com>
2024-08-26 16:55:56 -0300

    Adding basic structure and posts index

---
+++ src/componentes/MenuLink/index.js
@@ -0,0 +1,12 @@
+import { NavLink, useLocation } from 'react-router-dom'
+import styles from './MenuLink.module.css'
+
+export default function MenuLink({ children, to }) {
+  return(
+    <NavLink className={({isActive}) => `
+      ${styles.link} ${isActive ? styles.active : ''}
+    `} to={to}>
+    {children}
+  </NavLink>
+  )
+}
